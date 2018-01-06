a20c0eed434d09b2ff593d7ac29ff9db42f68c31
Victor Dumbrava <vdxcoder123@gmail.com>
2018-01-06 21:28:43 +0200

    Update Triangularity.py

--- Triangularity.py
+++ Triangularity.py
@@ -141,13 +141,6 @@ def run(code):
                 stack.append(chr(stack.pop()))
             elif command == "o":
                 stack.append(ord(stack.pop()))
-            elif command == ",":
-                a = stack[-1]
-                b = stack[-2]
-                stack = stack[:-2]
-                stack.append([a, b])
-            elif command == "N":
-                print(stack)
         else:
             if command != '"':
                 stack.append(stack.pop() + command)
